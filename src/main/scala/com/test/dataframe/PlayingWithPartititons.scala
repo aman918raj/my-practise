9a244fd2390147568b27227d6dec37fe08e2ff85
Aman Raj <43367687+aman918raj@users.noreply.github.com>
2023-02-28 20:59:59 +0530

    PlayingWithPartitions.scala edited

--- src/main/scala/com/test/dataframe/PlayingWithPartititons.scala
+++ src/main/scala/com/test/dataframe/PlayingWithPartititons.scala
@@ -10,12 +10,12 @@ object PlayingWithPartititons {
 
   def createSparkSession(num_shuffle_partitions: Int): DataFrame ={
     val spark = SparkSession.builder().master("local").config("spark.sql.shuffle.partitions",num_shuffle_partitions).appName("test").getOrCreate()
-    val df = spark.read.option("header", "true").csv("C:\\Users\\araj34\\Documents\\Learning\\spark\\files\\emp.csv")
+    val df = spark.read.option("header", "true").csv("C:\\Users\\<user_name>\\Documents\\Learning\\spark\\files\\emp.csv")
     return df
   }
   def main(args: Array[String]) : Unit={
 
-   val props = System.setProperty("hadoop.home.dir", "C:\\Users\\araj34\\hadoop")
+   val props = System.setProperty("hadoop.home.dir", "C:\\Users\\<user_name>\\hadoop")
     val df = createSparkSession(2)
     val parts = df.rdd.getNumPartitions
     println(parts)
