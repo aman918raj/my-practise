a346cac0cd86d0369557fe7bc6deb5a315d8cb42
Aman Raj <43367687+aman918raj@users.noreply.github.com>
2023-02-28 21:02:09 +0530

    RenameColumns.scala edited

--- src/main/scala/com/test/dataframe/RenameColumns.scala
+++ src/main/scala/com/test/dataframe/RenameColumns.scala
@@ -7,7 +7,7 @@ object RenameColumns {
   def main(args: Array[String]): Unit ={
 
     val spark = SparkSession.builder().master("local").appName("test").getOrCreate()
-    val df = spark.read.option("header", "true").csv("C:\\Users\\araj34\\Documents\\Learning\\spark\\files\\emp.csv")
+    val df = spark.read.option("header", "true").csv("C:\\Users\\<user_name>\\Documents\\Learning\\spark\\files\\emp.csv")
     val columnName = df.columns
     val newColumnName = columnName.map(x => f"${x}_${columnName.indexOf(x)}")
     val newColumnNameDf = df.toDF(newColumnName:_*)
